7ccf8a6f6cc4e025df565c29a3690387a29b5f66
YeongHun Cha <cyh0123q@gmail.com>
2016-06-27 00:48:02 +0900

    add icon_select
    * 아이콘 클릭 시 주황색으로 클릭 효과 추가
    * 아이콘 클릭 세션 값 저장

--- client/js/icon_list.js
+++ client/js/icon_list.js
@@ -1,6 +1,37 @@
 Template.icon_list.helpers({
     icon_main : function () {
-        alert(TE.find({part:'main'}).count());
         return TE.find({part:'main'});
+    },
+
+    icon_sub: function(){
+        return TE.find({part:'sub'});
     }
 });
+
+Template.icon.maybe_selected = function(){
+    return Session.equals('select_icon', this._id)? 'orange600':"";
+}
+Template.sub_icon.sub_maybe_selected = function(){
+    return Session.equals('sub_select_icon', this._id)? 'orange600':"";
+}
+Template.icon_list.events({
+   'click #main_icon_1':function(){
+       Session.set('select_icon', this._id);
+   },
+    'click #sub_icon_1':function(){
+        Session.set('sub_select_icon', this._id);
+    }
+});
+
+Template.show_icon.helpers({
+    show: function(){
+        var Icon_id =Session.get('select_icon');
+        return TE.find({_id:Icon_id});
+    },
+    sub_show: function(){
+        var Icon_id =Session.get('sub_select_icon');
+        return TE.find({_id:Icon_id});
+    }
+});
+
+
---
+++ server/init.js
@@ -0,0 +1,40 @@
+Meteor.startup(function () {
+    if (TE.find({part:'main'}).count() == 0) {
+        //alert("!!");
+        //alert("카운트 갯수"+TE.find({part:'main'}).count());
+        TE.insert({part: "main", name: 'location_on', sect: 'where'});
+        TE.insert({part: "main", name: 'schedule', sect: 'times'});
+        TE.insert({part: "main", name: 'attach_money', sect: 'money'});
+
+        //'어디' 섹션
+        TE.insert({part: "sub", name: 'wc', sect: 'where'}); // 화장실 아이콘
+        TE.insert({part: "sub", name: 'local_gas_station', sect: 'where'}); // 주유소 아이콘
+        TE.insert({part: "sub", name: 'local_parking', sect: 'where'}); // 주차장 아이콘
+        TE.insert({part: "sub", name: 'local_post_office', sect: 'where'}); // 우체국 아이콘
+        TE.insert({part: "sub", name: 'fitness_center', sect: 'where'}); // 피트니스센터(헬스장) 아이콘
+        TE.insert({part: "sub", name: 'wifi', sect: 'where'}); // 와이파이 아이콘
+        TE.insert({part: "sub", name: 'local_pharmacy', sect: 'where'}); // 약국 아이콘
+        TE.insert({part: "sub", name: 'smoking_rooms', sect: 'where'}); // 흡연실 아이콘
+
+//'언제' 섹션
+        TE.insert({part: "sub", name: 'flight_land', sect: 'times'}); // 비행기착륙 아이콘
+        TE.insert({part: "sub", name: 'flight_takeoff', sect: 'times'}); //비행기이륙 아이콘
+        TE.insert({part: "sub", name: 'directions_bus', sect: 'times'}); // 버스 아이콘
+        TE.insert({part: "sub", name: 'build', sect: 'times'}); // 스패너 아이콘(고치기)
+        TE.insert({part: "sub", name: 'directions_boat', sect: 'times'}); // 배 아이콘
+        TE.insert({part: "sub", name: 'directions_subway', sect: 'times'}); // 지하철 아이콘
+        TE.insert({part: "sub", name: 'movie', sect: 'times'}); // 영화 아이콘
+        TE.insert({part: "sub", name: 'call', sect: 'times'}); // 전화 아이콘
+
+//'얼마' 섹션
+        TE.insert({part: "sub", name: 'hotel', sect: 'money'}); //호텔(숙식) 아이콘
+        TE.insert({part: "sub", name: 'local_bar', sect: 'money'}); // 칵테일(술) 아이콘
+        TE.insert({part: "sub", name: 'local_cafe', sect: 'money'}); // 커피 아이콘
+        TE.insert({part: "sub", name: 'restaurant', sect: 'money'}); // 레스토랑(먹는것) 아이콘
+        TE.insert({part: "sub", name: 'local_florist', sect: 'money'}); // 꽃 아이콘
+        TE.insert({part: "sub", name: 'local_hospital', sect: 'money'}); // 의료비 아이콘
+        TE.insert({part: "sub", name: 'motorcycle', sect: 'money'}); // 오토바이 아이콘
+        TE.insert({part: "sub", name: 'local_taxi', sect: 'money'}); // 택시 아이콘
+
+    }
+});
